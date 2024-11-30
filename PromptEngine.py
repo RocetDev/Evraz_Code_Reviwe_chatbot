44f08a78b3d3448dda7171e0f6e7dc94e65f808c
RocetDev <curyk39@gmail.com>
2024-11-30 18:19:54 +0500

    Made Prompt Engine to model with custom and defaults prompts

---
+++ PromptEngine.py
@@ -0,0 +1,65 @@
+import os
+import requests
+import json
+
+class PEngine:
+    def __init__(self, model_api, api_key, temperature=0.35, max_tokens=1024):
+        self.model_api = model_api
+        self.api_key = api_key
+        self.max_tokens = max_tokens
+        self.temperature = temperature
+
+        self.headers = {
+            "Authorization": self.api_key,
+            "Content-Type": "application/json; charset=utf-8"
+        }
+
+        self.system_prompt = {
+            "role": "system",
+            "content": "Отвечайте на русском языке."
+        }
+
+    def set_system_content(self, text):
+        self.system_prompt['content'] = text
+
+    def _prepare_data(self, user_input):
+        data = {
+            "model": "mistral-nemo-instruct-2407",
+            "messages": [
+                self.system_prompt,
+                {
+                    "role": "user",
+                    "content": user_input
+                }
+            ],
+            "max_tokens": self.max_tokens,
+            "temperature": self.temperature
+        }
+        return data
+
+    def custom_query(self, text):
+        data = self._prepare_data(text)
+        response = requests.post(
+            self.model_api,
+            headers=self.headers,
+            data=json.dumps(data)
+        )
+        if response.status_code != 200:
+            raise Exception(f"Request failed with status code: {response.status_code}")
+        response_json = response.json()
+        content = response_json.get('choices')[0].get("message").get('content')
+        return content
+
+    def check_structure_dir(self, directory):
+        expected_files = ["requirements.txt", "README.md"]
+        actual_files = os.listdir(directory)
+
+        missing_files = set(expected_files) - set(actual_files)
+        extra_files = set(actual_files) - set(expected_files)
+
+        report = {
+            "missing_files": list(missing_files),
+            "extra_files": list(extra_files)
+        }
+
+        return report
--- main.py
+++ main.py
@@ -3,86 +3,106 @@ import asyncio
 from telebot.async_telebot import AsyncTeleBot
 import requests
 import json
+import glob
+import zipfile
+import os
+import tempfile
+import shutil
+
+from PromptEngine import PEngine
 
 
-# https://ru.stackoverflow.com/questions/988985/python-%d0%ba%d0%be%d0%b3%d0%b4%d0%b0-%d1%81%d0%bb%d0%b5%d0%b4%d1%83%d0%b5%d1%82-%d0%b8%d1%81%d0%bf%d0%be%d0%bb%d1%8c%d0%b7%d0%be%d0%b2%d0%b0%d1%82%d1%8c-async-%d0%b0-%d0%ba%d0%be%d0%b3%d0%b4%d0%b0-await/1070892#1070892
-# https://mastergroosha.github.io/aiogram-3-guide/quickstart/
 # https://habr.com/ru/companies/skillfactory/articles/837366/
 
 
 # Config для подключения к ЯЗ модели
-model_api = '<Your api>'
+model_api = '<Your url>'
 api_key = "<Your api key>"
 
 # Замените 'YOUR_TOKEN' на токен вашего бота
-API_TOKEN = '<Your Token bot>'
+API_TOKEN = '<Your token>'
 bot = AsyncTeleBot(API_TOKEN)
 
-
-async def analyze_file(file_path):
-    """Функция для анализа текстового файла."""
-    with open(file_path, 'r', encoding='utf-8') as f:
-        lines = f.readlines()
-        line_count = len(lines)
-        word_count = sum(len(line.split()) for line in lines)
-    
-    return f'Количество строк: {line_count}\nКоличество слов: {word_count}'
+propmt_engine = PEngine(model_api, api_key,)
 
 
 @bot.message_handler(commands=['start', 'help'])
 async def send_welcome(message):
-    await bot.reply_to(message, "Привет! Отправь мне текстовый файл, и я его проанализирую.")
+    text= "Привет! Отправь мне текстовый файл, и я его проанализирую."
+    await bot.reply_to(message=message, text=text, parse_mode='Markdown')
 
 
 @bot.message_handler(content_types=['text'])
 async def handle_text(message):
-    headers = {
-        "Authorization" : api_key,
-        "Content-Type": "application/json; charset=utf-8"
-    }
+    
 
-    data = {
-        "model": "mistral-nemo-instruct-2407",
-        "messages": [
-            {
-                "role": "system",
-                "content": "отвечай на русском языке"
-            },
-            {
-                "role": "user",
-                "content": message.text
-            }
-        ],
-        "max_tokens": 1000,
-        "temperature": 0.35
-    }
+    wait_text = "⏳ Подождите...  💭 Что-бот *EvraszGPT* генерирует ответ на ваш запрос..."
+    await bot.send_message(chat_id=message.chat.id, text=wait_text, parse_mode='Markdown')
 
-    await bot.send_message(message.chat.id, "⏳ Подождите...  💭 Что-бот EvraszGPT генерирует ответ на ваш запрос...")
+    try:
+        content = propmt_engine.custom_query(message.text)
 
-    response = requests.post(model_api, headers=headers, data=json.dumps(data)).json()
-    content = response.get('choices')[0].get("message").get('content')
+        await bot.delete_message(message.chat.id, message.message_id+1)
+        await bot.send_message(chat_id=message.chat.id, text=content, parse_mode='Markdown')
+    except Exception:
+        await bot.delete_message(message.chat.id, message.message_id+1)
+        await bot.send_message(message.chat.id, "Простите. Произошла ошибка, попробуйте снова.")
 
-    await bot.delete_message(message.chat.id, message.message_id+1)
-    await bot.send_message(message.chat.id, content)
 
+async def unzip_file(zip_filepath, extract_dir):
+    with zipfile.ZipFile(zip_filepath, 'r') as zf:
+        zf.extractall(extract_dir)
+
+
+async def walk_directory(directory: str):
+    """Рекурсивно обходит дерево директорий и возвращает его в виде словаря."""
+    tree = {}
+    for root, dirs, files in os.walk(directory):
+        # Определяем текущий узел в дереве
+        current_node = tree
+        path_parts = root[len(directory):].strip(os.sep).split(os.sep)
+        for part in path_parts:
+            if part not in current_node:
+                current_node[part] = {}
+            current_node = current_node[part]
+        # Добавляем файлы в текущий узел
+        current_node["files"] = files
+    return tree
+
+
+def is_zip_file(filename):
+    if filename[-3:] == 'zip':
+        return True
+    return False
 
 @bot.message_handler(content_types=['document'])
-async def handle_document(message):
+async def handle_codes(message):
+    file_name = message.document.file_name
     file_info = await bot.get_file(message.document.file_id)
     downloaded_file = await bot.download_file(file_info.file_path)
 
-    # Сохраняем файл на сервере
-    file_path = 'uploaded_file.txt'
-    with open(file_path, 'wb') as new_file:
-        new_file.write(downloaded_file)
+    # Сохраняем файл во временной директории
+    temp_dir = tempfile.mkdtemp(dir='FILES/')
+    file_path = os.path.join(temp_dir, file_name)
+    with open(file_path, 'wb') as f:
+        f.write(downloaded_file)
+    
+    wait_text = "⏳ Подождите...  💭 Что-бот *EvraszGPT* генерирует ответ на ваш запрос..."
+    await bot.send_message(chat_id=message.chat.id, text=wait_text, parse_mode='Markdown')
 
-    await process_file(file_path, message.chat.id)
+    if not is_zip_file(file_name):
+        await bot.delete_message(message.chat.id, message.message_id+1)
 
+        await bot.reply_to(message, "Простите, это не *.zip* файл")
+        await shutil.rmtree(temp_dir, ignore_errors=True) 
+    else:
+        await bot.delete_message(message.chat.id, message.message_id+1)
 
-async def process_file(file_path, chat_id):
-    """Функция для обработки файла и отправки результата пользователю."""
-    analysis_result = await analyze_file(file_path)
-    await bot.send_message(chat_id, f'Результат анализа:\n{analysis_result}')
+        await unzip_file(file_path, temp_dir)
+        os.remove(file_path)
+        project_file = os.path.join(temp_dir, os.listdir(temp_dir)[0])
+        tree = await walk_directory(project_file)
+        await bot.reply_to(message, tree)
 
 
 if __name__ == '__main__':
